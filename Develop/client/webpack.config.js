1b951cda3b2a048f72a21571e7dbaa1dce32b8c9
Gavin Wentzel <gavinwentzel@gmail.com>
2022-04-09 23:02:12 -0600

    Updated webpack config file and database/src-sw js files

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -18,13 +18,64 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      title: "J.A.T.E. - Just Another Text Editor",
+    }),
+
+    
+    new InjectManifest({
+      swSrc: "./src-sw.js",
+      swDest: "src-sw.js",
+    }),
+
+    // Creates a manifest.json file.
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: "J.A.T.E. - Just Another Text Editor",
+      short_name: "J.A.T.E.",
+      description: "Code snippets",
+      background_color: "#FFFFFF",
+      theme_color: "#FFFFFF",
+      start_url: "/",
+      publicPath: "/",
+      icons: [
+        {
+          src: path.resolve("src/images/logo.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
+        },
+      ],
+    }),
       
     ],
 
     module: {
       rules: [
-        
-      ],
+       
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: "asset/resource",
+          },
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: ["@babel/preset-env"],
+                plugins: [
+                  "@babel/plugin-proposal-object-rest-spread",
+                  "@babel/transform-runtime"],
+                  },
+              },
+          },
+        ],
     },
   };
 };
